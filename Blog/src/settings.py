258c01801c30cd7406489656f6a28486a55e042c
Londa-LG <66629166+Londa-LG@users.noreply.github.com>
2020-09-06 14:39:12 +0200

    Heroku Deploy ready

--- Blog/src/settings.py
+++ Blog/src/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'tlgeo%f+r%$8vih#kaj2#-y)u*knjd=a6b&cn(=&t__lu2ed%&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1:8000']
 
 
 # Application definition
